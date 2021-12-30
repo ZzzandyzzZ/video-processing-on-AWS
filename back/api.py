84b1f3697d406e77e265cb11feb5647101d377e6
Eduardo Sanchez <esanchez.h25@gmail.com>
2021-12-30 12:25:23 -0500

    fixes

--- back/api.py
+++ back/api.py
@@ -21,29 +21,12 @@ s3 = boto3.resource('s3',
     region_name='us-west-2'
 )
 
-
-
 client = MongoClient('mongodb://mongodb:27017/')
 db = client.VideoProcessingDB
 
+
 @app.route('/')
 def hello_world():
-    """
-    data = [{1: {'person': 1, 'oven': 0, 'cup': 0, 'toaster': 0}}, {2: {'person': 1, 'elephant': 0, 'cup': 0, 'teddy bear': 0, 'bowl': 0, 'umbrella': 0, 'potted plant': 0, 'vase': 0, 'broccoli': 0, 'cat': 0, 'dog': 0, 'bird': 0}}, {3: {'person': 1, 'sink': 0, 'sports ball': 0, 'dog': 0, 'cup': 0, 'car': 0, 'toilet': 0}}, {4: {'person': 0, 'refrigerator': 0, 'fire hydrant': 0, 'dog': 0, 'cat': 0, 'umbrella': 0, 'broccoli': 0, 'potted plant': 0, 'cake': 0, 'boat': 0, 'bird': 0, 'carrot': 0, 'teddy bear': 0, 'donut': 0}}, {5: {'cake': 0, 'cat': 0, 'donut': 0, 'umbrella': 0, 'dog': 0, 'teddy bear': 0, 'person': 0, 'apple': 0, 'potted plant': 0, 'broccoli': 0, 'remote': 0, 'handbag': 0}}, {6: {'potted plant': 1, 'cake': 0, 'handbag': 0}}, {7: {'potted plant': 0, 'cake': 0}}]
-    estructure_data = {}
-    for item in data:
-        for values in list(item.values())[0].keys():
-            if values not in estructure_data:
-                estructure_data[values] = [list(item.keys())[0]]
-            else:
-                estructure_data[values].append(list(item.keys())[0])
-    new_estructure = []
-    for item,value in estructure_data.items():
-        new_estructure.append({"tag":item,"min":value})
-
-    print('This is error output',new_estructure, file=sys.stderr)
-    return (new_estructure)
-    """
     return "BACKEND woring fine!"
 
 
@@ -56,25 +39,6 @@ def search():
     s3.Object(s3_bucket_name,'videos/'+f.filename).upload_file(f.filename)
     #s3.Object(s3_bucket_name,'videos/'+ f.filename).download_file(f.filename)
     return {"response":"TODO OK"}
-
-
-@app.route('/video')
-def users():
-    collection = db.videos.find()
-
-    item = {}
-    data = []
-    for element in collection:
-        item = {
-            'id': str(element['_id']),
-            'name': element['name'],
-            'url': element['url']
-        }
-        data.append(item)
-
-    return jsonify(
-        data=data
-    )
 
 
 @app.route('/save-video', methods=['POST'])
@@ -154,18 +118,6 @@ def search_video():
     response_data = json.loads(response.content.decode())
     return jsonify(response_data)
 
-
-@app.route('/user')
-def user():
-    name = request.args.get('name')
-    url = request.args.get('url')
-    video = {
-        'name': name,
-        'url': url
-    }
-    db.videos.insert_one(video)
-
-    return 'Saved!', 201
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
