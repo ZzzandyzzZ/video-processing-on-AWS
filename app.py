283f23608432f373472a813dfe97937ad54ba946
Eduardo Sanchez <esanchez.h25@gmail.com>
2021-12-29 23:50:09 -0500

    create Video model

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from flask_mongoengine import MongoEngine
 
+from models.video import Video
+
 app = Flask(__name__)
 app.config["MONGODB_SETTINGS"] = {
     'db': 'cloud_unsa'
 }
 db = MongoEngine(app)
 
-@app.route('/', methods=['GET'])
+@app.route('/test', methods=['GET'])
 def test():
+    Video(
+        name='asd',
+        duration=20.4,
+        url='qweqwe'
+    ).save()
     return {
         'success': True
     }
